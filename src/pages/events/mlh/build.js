68075fe2eb07e49fb9b09196305b24bf78b43ab2
sanjithpk <sanjithpk06@gmail.com>
2020-01-23 13:57:40 +0530

    removing all files

--- src/pages/events/mlh/build.js
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import Mlhbuildday from "../../../components/events/mlh-build-day"
-
-const Events = () => (
-  < Mlhbuildday/>
-)
-
-export default Events
--- src/pages/events/mlh/hack.js
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import Mlhhackday from "../../../components/events/mlh-hack-day"
-
-const Events = () => (
-  < Mlhhackday/>
-)
-
-export default Events
